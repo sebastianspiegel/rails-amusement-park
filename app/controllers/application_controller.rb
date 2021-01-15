3c5949a2b7c1b2e2a6ce689687e45f5d3e4a576d
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-01-15 15:18:35 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helpers do 
+
+    def current_user_is_admin?
+      User.find(session[:user_id]).admin
+    end
+
+  end
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,33 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction 
+    belongs_to :user 
+
+    def take_ride
+        if self.user.tickets < self.attraction.tickets || self.user.height < self.attraction.min_height
+            cannot_ride
+        else
+            self.user.decrement!(:tickets, by = attraction.tickets)
+            self.user.increment!(:happiness, by = attraction.happiness_rating)
+            self.user.increment!(:nausea, by = attraction.nausea_rating)
+            "Thanks for riding the #{attraction.name}!"
+        end
+    end
+
+    def cannot_ride
+        if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+            "Sorry. " + not_enough_tickets + " " + not_tall_enough
+        elsif user.tickets < attraction.tickets
+            "Sorry. " + not_enough_tickets
+        else
+            "Sorry. " + not_tall_enough
+        end
+    end
+
+    def not_enough_tickets
+        "You do not have enough tickets to ride the #{self.attraction.name}."
+    end
+
+    def not_tall_enough
+        "You are not tall enough to ride the #{self.attraction.name}."
+    end
 end
